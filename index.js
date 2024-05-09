cd1f41e32ddf310cc15e3424909710256fc689e2
Divya <karkidivya5@gmail.com>
2024-05-09 21:05:29 +0545

    carousel-added

--- index.js
+++ index.js
@@ -69,3 +69,25 @@ document.getElementById("blog1").addEventListener("click", function() {
     window.open("Blogs/blog1.html", "_blank"); // Replace "https://www.example.com" with the URL of the page you want to open
   });
   
+
+
+//   carousel section
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
